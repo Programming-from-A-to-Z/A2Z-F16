02a0fb159e4899fc3e42172646569550260942da
Daniel Shiffman <daniel@shiffman.net>
2016-10-10 20:52:47 -0400

    adding example to download image

---
+++ week4-twitter/10_download_image_url/app.js
@@ -0,0 +1,26 @@
+// A2Z F16
+// Daniel Shiffman
+// http://shiffman.net/a2z
+// https://github.com/shiffman/A2Z-F16
+
+
+var url = 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg';
+
+var fs = require('fs');
+var request = require('request');
+
+download(url, 'kitten.jpg');
+
+function download(url, filename) {
+  request.head(url, downloading);
+
+  function downloading(err, res, body) {
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+    request(url).pipe(fs.createWriteStream(filename)).on('close', finished);
+  }
+
+  function finished() {
+    console.log('downloaded to: ' + filename);
+  }
+}
